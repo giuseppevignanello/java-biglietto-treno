75d249c6a9a6441474ada4b02d134ebb1fa7003c
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-13 16:18:51 +0200

    set user price

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,22 @@
 package org.lessons.java;
 
-public class CalcolaBiglietto {
+import java.util.Scanner;
 
+public class CalcolaBiglietto {
+	public static void main(String[] args) {
+		
+		double pricePerKm = 0.21;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Quanti kilometri devi percorrere?");
+		
+		int userKm = sc.nextInt();
+		
+		double userPrice = userKm * pricePerKm; 
+		System.out.println(userPrice);
+		
+		
+		
+	}
 }
